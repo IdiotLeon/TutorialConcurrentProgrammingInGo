663f196a6aa1fa3decff1d88546e2fe0219daa0c
Yang Lu <Leon@leonpro.guest.corp.microsoft.com>
2018-10-02 16:58:52 -0700

    demo: Adding a live&small log system with time stamps

--- module3/3.1MutexLockWithGoroutine/3.1MutexLockWithGoroutine.go
+++ module3/3.1MutexLockWithGoroutine/3.1MutexLockWithGoroutine.go
@@ -2,31 +2,51 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
-	"sync"
+	"time"
 )
 
-/**
- *	However, the data is mostly ordered,
- *	hinting the process is no long running in parallel,
- *	as much as it might first appear.
- *	This is caused by the locking and unlocking.
- *	As a matter of fact,
- *	this application actually performs more slowly than a single threaded model
- *	due to all of the synchronization.
- */
+// To simulate Mutex using Channels
 func main() {
-	mutex := new(sync.Mutex)
 	runtime.GOMAXPROCS(4)
+
+	// Previous logs will be erased due to new creations
+	f, _ := os.Create("./log.txt")
+	f.Close()
+
+	// To give a large, nice buffer to try and prevent our application
+	// from becoming IO bound,
+	// since writing to the disk is going to be much slower than our calculation
+	logCh := make(chan string, 50)
+
+	go func() {
+		for {
+			msg, ok := <-logCh
+			if ok {
+				f, err := os.OpenFile("./log.txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+				if err != nil {
+					fmt.Println(err)
+				}
+				logTime := time.Now().Format(time.RFC3339)
+				f.WriteString(logTime + " - " + msg)
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
+
+	mutex := make(chan bool, 1)
 
 	for i := 1; i < 10; i++ {
 		for j := 1; j < 10; j++ {
-			// To obtain the lock on the mutex
-			mutex.Lock()
+			mutex <- true
 			go func() {
-				fmt.Printf("%d + %d = %d\n", i, j, i+j)
-				// To unlock the mutext allowing the next goroutine to be processed				mutex.Unlock()
-				mutex.Unlock()
+				msg := fmt.Sprintf("%d + %d = %d\n", i, j, i+j)
+				logCh <- msg
+				fmt.Print(msg)
+				<-mutex
 			}()
 		}
 	}
